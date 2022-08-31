b9328aa3b543ef7e3c76405781c22c1ab2487621
ravi11o <ravi.suraj110@gmail.com>
2022-08-31 10:30:56 +0530

    Stack and Queue

---
+++ block-BJaaii/code/index.js
@@ -0,0 +1,20 @@
+class Stack{
+  constructor(stack){
+   this.stack=stack;
+  }
+
+  push(newstack){
+    this.stack=newstack;
+    return this.stack;
+  }
+  pop(){
+
+  }
+  peek(){
+
+  }
+  reverse(){
+    
+  }
+ 
+}
